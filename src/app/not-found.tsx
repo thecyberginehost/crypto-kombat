e972b7f45180a6b55f2e42a5d4e16e84ee975fe7
thecyberginehost <aamore@cyberginehosting.com>
2025-02-04 15:11:52 -0500

    Fix not-found.tsx escaping & link issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,17 @@
+"use client";
+
+import Link from "next/link";
+
 export default function NotFound() {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center text-white bg-black">
-        <h1 className="text-5xl font-bold text-red-500">404: Page Not Found</h1>
-        <p className="mt-4 text-lg text-gray-400">This page doesn't exist. Try going back to the homepage.</p>
-        <a href="/" className="mt-6 px-6 py-3 bg-yellow-500 text-black font-bold rounded-lg">Go Home</a>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-white bg-gray-900 p-6">
+      <h1 className="text-5xl font-extrabold text-red-500">404 - Page Not Found</h1>
+      <p className="mt-4 text-lg text-gray-300">
+        Sorry, the page you&apos;re looking for doesn&apos;t exist.
+      </p>
+      <Link href="/" className="mt-6 px-6 py-3 text-lg font-bold text-black bg-yellow-500 hover:bg-yellow-600 rounded-lg transition duration-300">
+        Return Home
+      </Link>
+    </div>
+  );
+}
